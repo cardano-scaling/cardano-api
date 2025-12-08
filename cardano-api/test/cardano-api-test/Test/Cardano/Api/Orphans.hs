4b0d0399ba84d6da420e0f602c4af9060b2bd0f0
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2025-12-08 19:40:08 +0100

    WIP
    Signed-off-by: Sasha Bogicevic <sasha.bogicevic@iohk.io>

--- cardano-api/test/cardano-api-test/Test/Cardano/Api/Orphans.hs
+++ cardano-api/test/cardano-api-test/Test/Cardano/Api/Orphans.hs
@@ -14,16 +14,7 @@ module Test.Cardano.Api.Orphans () where
 import Cardano.Api.Byron
 import Cardano.Api.Key
 
-import Cardano.Crypto qualified as Crypto
-import Cardano.Ledger.BaseTypes qualified as L
-import Cardano.Ledger.Credential (Ptr (..), SlotNo32 (..), StakeReference (..))
-
-import Data.Word
-
 import Test.Gen.Cardano.Api.Orphans ()
-import Test.Gen.Cardano.Api.Typed ()
-
-import Test.QuickCheck
 
 -- Signing Key instances
 
@@ -45,27 +36,4 @@ deriving instance Eq (SigningKey KesKey)
 
 deriving instance Eq (SigningKey VrfKey)
 
-instance Arbitrary StakeReference where
-  arbitrary = genStakeRefWith arbitrary
-  shrink = genericShrink
 
-deriving instance Arbitrary SlotNo32
-
-instance Arbitrary Ptr where
-  arbitrary = Ptr <$> arbitrary <*> arbitrary <*> arbitrary
-
-instance Arbitrary L.TxIx where
-  -- starting with Conway, we only deserialize TxIx within Word16 range
-  arbitrary = L.TxIx . fromIntegral <$> arbitrary @Word16
-
-instance Arbitrary L.CertIx where
-  -- starting with Conway, we only deserialize CertIx within Word16 range
-  arbitrary = L.CertIx . fromIntegral <$> arbitrary @Word16
-
-genStakeRefWith :: Gen Ptr -> Gen StakeReference
-genStakeRefWith genPtr =
-  frequency
-    [ (80, StakeRefBase <$> arbitrary)
-    , (5, StakeRefPtr <$> genPtr)
-    , (15, pure StakeRefNull)
-    ]
